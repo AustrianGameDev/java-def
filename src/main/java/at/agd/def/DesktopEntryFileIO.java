c72befd9a47e29fe6266c598e8afc502dbde720e
AustrianGameDev <austriangamedev@gmail.com>
2023-01-06 20:52:33 +0100

    Added DesktopEntryFileIO class

---
+++ src/main/java/at/agd/def/DesktopEntryFileIO.java
@@ -0,0 +1,14 @@
+package at.agd.def;
+
+public class DesktopEntryFileIO
+{
+    public static void safeDefToFile(DesktopEntryFile def, String pathStr, String filename)
+    {
+
+    }
+
+    public static DesktopEntryFile readDefFromFile(String pathStr)
+    {
+        return null;
+    }
+}
