0e7bf75bf850f70bf2254b891b725e8890e26622
AustrianGameDev <austriangamedev@gmail.com>
2023-01-06 17:04:16 +0100

    Added Key class

---
+++ src/main/java/at/agd/def/pojo/Key.java
@@ -0,0 +1,17 @@
+package at.agd.def.pojo;
+
+public class Key
+{
+    protected String name;
+
+    public Key(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "";
+    }
+}
