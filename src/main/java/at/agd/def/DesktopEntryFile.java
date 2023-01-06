026038699769bf17687fb3a3fecdb7bd36e3019d
AustrianGameDev <austriangamedev@gmail.com>
2023-01-06 16:59:31 +0100

    Added basic DesktopEntryFile

---
+++ src/main/java/at/agd/def/DesktopEntryFile.java
@@ -0,0 +1,14 @@
+package at.agd.def;
+
+public class DesktopEntryFile
+{
+    public DesktopEntryFile(int type, String name, String url)
+    {
+        init();
+    }
+
+    private void init()
+    {
+
+    }
+}
